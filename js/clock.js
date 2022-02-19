8a9d150267f675a88fe1063326dbe14138e2de33
kristijan_00 <kristijandjakovic10@gmail.com>
2022-02-19 19:17:02 +0100

    Added selection box for cities

--- js/clock.js
+++ js/clock.js
@@ -6,6 +6,7 @@ let hourHand = document.querySelector('[hour-hand]');
 
 function setClock() {
     const currentDate = new Date();
+
     const seconds = currentDate.getSeconds() / 60;
     const minutes = (seconds + currentDate.getMinutes()) / 60;
     const hours = (minutes + currentDate.getHours()) / 12;
